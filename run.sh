07af5c10ca91b49c63ad9df582dc813b35f45c69
Lordclintondevops <139438345+Lordclintondevops@users.noreply.github.com>
2024-05-22 08:56:19 -0700

    new project

---
+++ run.sh
@@ -0,0 +1,23 @@
+# Excuting the commands from here
+
+# terraform init
+
+terraform plan
+
+# terraform apply -auto-approve
+
+# sleep 120
+
+# terraform destroy -auto-approve
+
+# git init 
+
+# git branch -M main
+
+git add .
+
+git commit -m "new project"
+
+git remote add clinton https://github.com/Lordclintondevops/project11.git
+
+git push clinton main
